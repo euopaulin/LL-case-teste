b257efadcb73ac09f80425984da9b1865199d598
Paulo Henrique <paulohspfc209@gmail.com>
2025-07-23 08:50:13 -0300

    update

--- tests/test_ll.py
+++ tests/test_ll.py
@@ -1,5 +1,16 @@
+import os
+os.environ["HTTP_PROXY"] = ""
+os.environ["HTTPS_PROXY"] = ""
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--no-proxy-server")     # Ignora proxy de sistema
+options.add_argument('--proxy-server=')       # Zera explicitamente o proxy
+
+driver = webdriver.Chrome(options=options)
 
 # Inicia o navegador
 driver = webdriver.Chrome()
@@ -8,7 +19,14 @@ driver = webdriver.Chrome()
 driver.get("https://ll.ipea.gov.br/")
 
 # Interação de exemplo
-driver.find_element(By.CSS_SELECTOR, "submit").click()
+driver.find_element(By.ID, "username").send_keys("t07692665176s")
+
+driver.find_element(By.ID, "password").send_keys("Spfc202430")
+
+driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+
+driver.find_element(By.XPATH, '//button[contains(@onclick, "dadosdelogoff.php)]').click()
+
+
 
 # Encerra o navegador
-driver.quit()
